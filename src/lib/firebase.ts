0f9dc32305a9d9b7cd3c0106af2422c70425dffa
Soumyajit Mallick <soumyajitmallick0123@gmail.com>
2025-10-24 12:01:06 +0000

    reate the ,env local file to propely fetch and upload data to the fireba

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_KRERSfY510iFEVBSu0HGtqOY5pzkF1c",
-  authDomain: "studio-2514331212-b0f7f.firebaseapp.com",
-  projectId: "studio-2514331212-b0f7f",
-  storageBucket: "studio-2514331212-b0f7f.appspot.com",
-  messagingSenderId: "877736539149",
-  appId: "1:877736539149:web:1fc0d5406a63615b7ad144"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
