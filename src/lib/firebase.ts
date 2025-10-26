43e14a9e68cdb89fe448c6009381cd99d4995071
Amir Kumar Das <imamir760@gmail.com>
2025-10-26 08:39:31 +0000

    the pages are still staci here not copnnected to the proper firbase API

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyA_KRERSfY510iFEVBSu0HGtqOY5pzkF1c",
+  authDomain: "studio-2514331212-b0f7f.firebaseapp.com",
+  projectId: "studio-2514331212-b0f7f",
+  storageBucket: "studio-2514331212-b0f7f.appspot.com",
+  messagingSenderId: "877736539149",
+  appId: "1:877736539149:web:5c95543d9235df5b7ad144",
+  measurementId: "G-XXXXXXXXXX"
 };
 
 let app: FirebaseApp | null = null;
